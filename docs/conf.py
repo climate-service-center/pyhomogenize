56440bd5d8f92496b9a7cbee0d886af01e31a32b
Ludwig Lierhammer <ludwig.lierhammer@hereon.de>
2022-03-21 15:57:27 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,10 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.napoleon',
               'sphinxcontrib.mockautodoc',
-              'numpydoc'
+              'numpydoc',
+              'nbsphinx',
+              'nbsphinx_link',
+              'IPython.sphinxext.ipython_console_highlighting',
               ]
 
 autosummary_generate = True
@@ -85,7 +88,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
