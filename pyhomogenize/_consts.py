2e2f52b51617819ffe455a07de3c7a4e713c6004
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2023-02-22 10:31:40 +0100

    consts from pyhomogenize

--- pyhomogenize/_consts.py
+++ pyhomogenize/_consts.py
@@ -4,9 +4,27 @@ frequencies = {
     "6hr": "6H",
     "6hrPt": "6H",
     "day": "D",
+    "week": "7D",
     "mon": ["MS", "M"],
     "monClim": ["MS", "M"],
+    "sem": ["QS-DEC", "Q-FEB"],
     "yr": ["AS", "A"],
+    "year": ["AS", "A"],
+    "fx": None,
+}
+
+freqs = {
+    "1hr": "1H",
+    "3hr": "3H",
+    "6hr": "6H",
+    "6hrPt": "6H",
+    "day": "D",
+    "week": "7D",
+    "mon": "MS",
+    "monClim": "MS",
+    "sem": "QS-DEC",
+    "yr": "AS",
+    "year": "AS",
     "fx": None,
 }
 
@@ -16,28 +34,36 @@ translator = {
     "6hr": "hour",
     "6hrPt": "hour",
     "day": "day",
+    "week": "week",
     "mon": "month",
     "monClim": "month",
+    "sem": "month",
     "yr": "year",
+    "year": "year",
     "fx": None,
 }
 
-format = {
+fmt = {
     "1hr": "%Y-%m-%dT%H:%M",
     "3hr": "%Y-%m-%dT%H:%M",
     "6hr": "%Y-%m-%dT%H:%M",
     "6hrPt": "%Y-%m-%dT%H:%M",
     "day": "%Y-%m-%d",
+    "week": "%Y-%m-%d",
     "mon": "%Y-%m",
     "monClim": "%Y-%m",
+    "sem": "%Y-%m",
     "yr": "%Y",
+    "year": "%Y",
     "fx": None,
 }
 
 is_month = {
     "hour": True,
     "day": True,
+    "week": True,
     "month": False,
+    "sem": False,
     "year": False,
     None: False,
 }
@@ -70,6 +96,13 @@ equalize = {
         "microsecond",
         "nanosecond",
     ],
+    "week": [
+        "hour",
+        "minute",
+        "second",
+        "microsecond",
+        "nanosecond",
+    ],
     "mon": [
         "day",
         "hour",
@@ -86,7 +119,24 @@ equalize = {
         "microsecond",
         "nanosecond",
     ],
+    "sem": [
+        "day",
+        "hour",
+        "minute",
+        "second",
+        "microsecond",
+        "nanosecond",
+    ],
     "yr": [
+        "month",
+        "day",
+        "hour",
+        "minute",
+        "second",
+        "microsecond",
+        "nanosecond",
+    ],
+    "year": [
         "month",
         "day",
         "hour",
@@ -103,9 +153,12 @@ within = {
     "6hr": NotImplemented,
     "6hrPt": NotImplemented,
     "day": ["year", "month", "day"],
+    "week": ["year", "month", "day"],
     "mon": ["year", "month"],
     "monClim": ["year", "month"],
+    "sem": ["year", "month"],
     "yr": ["year"],
+    "year": ["year"],
     "fx": None,
 }
 
@@ -113,4 +166,19 @@ naming = {
     "duplicates": "duplicated_timesteps",
     "redundants": "redundant_timesteps",
     "missings": "missing_timesteps",
+}
+
+tbounds = {
+    "1hr": NotImplemented,
+    "3hr": NotImplemented,
+    "6hr": NotImplemented,
+    "6hrPt": NotImplemented,
+    "day": ["D", "D", 12],
+    "week": ["7D", "7D", 42],
+    "mon": ["MS", "M", 12],
+    "monClim": ["MS", "M", 12],
+    "sem": ["QS-DEC", "Q-FEB", 12],
+    "yr": ["AS", "A", 12],
+    "year": ["AS", "A", 12],
+    "fx": None,
 }
