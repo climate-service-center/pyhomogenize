0374da3828c7378a11ac2c9b4c02956bb7f73038
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2023-02-23 15:22:11 +0100

    asserts deleted

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,10 +7,10 @@ import pyhomogenize as pyh
 
 
 def test_check_existance():
-    assert pyh._utilities.check_existance([])
-    assert pyh._utilities.check_existance(["test.nc"])
+    pyh._utilities.check_existance([])
+    pyh._utilities.check_existance(["test.nc"])
 
 
 def test_get_operator():
-    assert pyh._utilities.get_operator(object="", name="")
-    assert pyh._utilities.get_operator(name="test")
+    pyh._utilities.get_operator(object="", name="")
+    pyh._utilities.get_operator(name="test")
