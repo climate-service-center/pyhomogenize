161376ab21ec0375af003145bdc8d6905c876263
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2023-03-23 09:06:04 +0100

    python version 3.8, 3.9, 3.10

--- setup.py
+++ setup.py
@@ -32,17 +32,16 @@ test_requirements = []
 setup(
     author="Ludwig Lierhammer",
     author_email="ludwig.lierhammer@hereon.de",
-    python_requires=">=3.5",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="Tool to homogenize netCDF files to CF standard",
     entry_points={
