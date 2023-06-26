e301446de674d0437a794d651f672d120e98382b
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2023-06-26 13:58:59 +0200

    typo

--- pyhomogenize/pyhomogenize.py
+++ pyhomogenize/pyhomogenize.py
@@ -1,19 +1,19 @@
 """Main module."""
 
-from . import _operators, _utilities
+from . import _utilities, operators
 
 
 def pyhomogenize(args):
     """Run pyhomogenize command-line interface."""
     if args.operators:
-        print(_operators.operators)
+        print(operators.operators)
         return
     operator = args.operator[0]
     arguments = args.operator[1:]
     args.operator = operator
     args.arguments = arguments
     # check if selected operator is available
-    func = _utilities.get_operator(_operators, args.operator, type="operator")
+    func = _utilities.get_operator(operators, args.operator, type="operator")
     if not func:
         return
     # check if all input files are available
