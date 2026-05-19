68dabd7210b1c6ef4d28ec1865fbb7da595fd70c
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2026-05-19 13:08:47 +0200

    Update Python version support in setup.py
    Removed support for Python 3.9 and 3.10, added support for Python 3.12, 3.13, and 3.14.

--- setup.py
+++ setup.py
@@ -39,9 +39,10 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     description="Tool to homogenize netCDF files to CF standard",
     entry_points={
